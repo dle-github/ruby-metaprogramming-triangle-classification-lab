9e9e39f2bdf6cf988cf15095d93ee7362732a665
derek <derekle.creative@gmail.com>
2021-02-13 04:22:45 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  def initialize(input1, input2, input3)
+    p "initialize class [Triangle] with side lenghts of [#{input1}], [#{input2}], and [#{input3}]"
+    @lenA = input1
+    @lenB = input2
+    @lenC = input3
+
+    @sides = Array.new
+    @sides << @lenA
+    @sides << @lenB
+    @sides << @lenC
+  end
+
+  def kind
+    if @sides.detect{|x| x <= 0} != nil
+        raise TriangleError
+    end
+    if @sides.max < (@sides.sum - @sides.max) == false
+      raise TriangleError
+    end
+
+    matches = @sides.select { |x| @sides.count(x) > 1}
+    p matches
+    p "there are [#{matches.length}] matches in @sides [#{@sides}]"
+    if matches.length == 3
+      return :equilateral
+    end
+    if matches.length == 2
+      return :isosceles
+    end
+    return :scalene
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
